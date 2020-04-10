e6a44b9ad580b2fd85793b3d53b04a2fa62e02f1
Sulav Khadka <khadkasulav@gmail.com>
2020-04-10 16:11:43 -0400

    taking out debug from the flask app start

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def spotify_login():
     spotify.user_login()
     
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(port=5000) 
 
     refresh_token_offset_in_seconds = int(os.environ["refresh_token_offset_in_seconds"])
     bio_update_time_interval_in_seconds = int(os.environ["update_time_interval_in_seconds"])
